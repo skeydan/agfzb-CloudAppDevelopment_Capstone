0823129c43f0902bc883eeb31ce53d5e70d9ef89
Sigrid Keydana <469371+skeydan@users.noreply.github.com>
2023-10-27 12:42:24 +0200

    Update get-dealership.js

--- functions/get-dealership.js
+++ functions/get-dealership.js
@@ -7,9 +7,10 @@ const Cloudant = require('@cloudant/cloudant');
 async function dbCloudantConnect() {
     try {
         const cloudant = Cloudant({
-            plugins: { iamauth: { iamApiKey: '' } }, // Replace with your IAM API key
-            url: '', // Replace with your Cloudant URL
+            plugins: { iamauth: { iamApiKey: 'p5rJrSqj01I7S5C2RiuYZKPiQXDrESYAuiLAhfL7uMe6' } }, // Replace with your IAM API key
+            url: 'https://3c5b9256-67d2-48dd-a2b6-9cbf727a75bf-bluemix.cloudantnosqldb.appdomain.cloud', // Replace with your Cloudant URL
         });
+
 
         const db = cloudant.use('dealerships');
         console.info('Connect success! Connected to DB');
